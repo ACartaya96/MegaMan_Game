7609bd7c58c85ab40f24d9709920e5fd8af239b8
ACartaya96 <87090729+ACartaya96@users.noreply.github.com>
2022-02-02 15:10:36 -0500

    Random Commits

---
+++ Assets/Scripts/BackgroundLERP.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLERP : MonoBehaviour
+{
+    PlayerController mm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mm = GetComponent<PlayerController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if()
+        {
+            //Lerp Code;
+        }
+    }
+}
